cb365b20cc97bd232ea38b8f7c5073ec53fcc7c5
Jose Duarte <duartematiasduarte@gmail.com>
2023-08-23 21:21:02 +0000

    10mo programa  con similar al anterior mostrando datos de Tablad dinamicas el dar clik en una fila

--- employees/webapp/controller/View1.controller.js
+++ employees/webapp/controller/View1.controller.js
@@ -64,6 +64,103 @@ sap.ui.define([
         oJsonModelConfig.setProperty("/visibleBtnHideCity",false);
      }
 
+     function showOrders(oEvent){
+      var ordersTable = this.getView().byId("ordersTable")
+       ordersTable.destroyItems();
+
+       var itemPressed = oEvent.getSource();
+       var oContext = itemPressed.getBindingContext("jsonEmployees");
+       var object = oContext.getObject();
+       var orders = object.Orders;
+
+       var orderItems= [];
+
+       for (var i in orders){
+        orderItems.push( new sap.m.ColumnListItem({
+           cells : [
+             new sap.m.Label({text :orders[i].OrderID}),
+             new sap.m.Label({text :orders[i].Freight}),
+             new sap.m.Label({text :orders[i].ShipAddress}),
+           ]
+        }))
+       }
+         /**  */
+       var newTable = new sap.m.Table({
+        width:"auto",
+        columns :[
+             new sap.m.Column({
+              header : new sap.m.Label({ 
+                text:"{i18n>orderID}"
+              })
+            }),
+            new sap.m.Column({
+              header : new sap.m.Label({ 
+                text:"{i18n>freight}"
+              })
+            }),
+            new sap.m.Column({
+              header : new sap.m.Label({ 
+                text:"{i18n>shipAddress}"
+              })
+            })
+              
+        ],
+        items : orderItems
+        } ).addStyleClass("sap.UiSmallMarginEnd")
+       ordersTable.addItem(newTable);
+  //////nueva tabla hecha con JS
+  var newTableJSON =new sap.m.Table();
+  newTableJSON .setWidth("auto");
+  newTableJSON .addStyleClass("sapUiTinyMarginEnd"); 
+
+  var columnOrderID =new sap.m.Column();
+  var labelOrderID =new sap.m.Label(); 
+  labelOrderID.bindProperty("text","i18n>orderID");
+  columnOrderID.setHeader(labelOrderID);
+  newTableJSON.addColumn(columnOrderID)
+
+  var columnFreight =new sap.m.Column();
+  var labelFreight =new sap.m.Label(); 
+  labelFreight.bindProperty("text","i18n>freight");
+  columnFreight.setHeader(labelFreight);
+  newTableJSON.addColumn(columnFreight)
+
+  var columnShipAddress =new sap.m.Column();
+  var labelShipAddress =new sap.m.Label(); 
+  labelShipAddress.bindProperty("text","i18n>shipAddress");
+  columnShipAddress.setHeader(labelShipAddress);
+  newTableJSON.addColumn(columnShipAddress);
+
+  
+
+  var columnListItem = new sap.m.ColumnListItem();
+  
+  var cellOrderID = new sap.m.Label();
+  cellOrderID.bindProperty("text","jsonEmployees>OrderID");
+  columnListItem.addCell(cellOrderID)
+
+  var cellFreight = new sap.m.Label();
+  cellFreight.bindProperty("text","jsonEmployees>Freight");
+  columnListItem.addCell(cellFreight);
+
+  var cellShipAddress = new sap.m.Label();
+  cellShipAddress.bindProperty("text","jsonEmployees>ShipAddress");
+  columnListItem.addCell(cellShipAddress);
+
+  var oBindingInfo = {
+   model: "jsonEmployees",
+   path: "Orders",
+   template: columnListItem
+  };
+
+   newTableJSON.bindAggregation( "items", oBindingInfo);
+   newTableJSON.bindElement( "jsonEmployees>" + oContext.getPath());
+   ordersTable.addItem(newTableJSON);
+
+         
+
+     }
+
         return Controller.extend("logaligroup.employees.controller.View1", {
             onAfterRendering: function () {
               var oView = this.getView();
@@ -105,7 +202,8 @@ sap.ui.define([
             onClearFilter:onClearFilter,
             showPostalCode:showPostalCode,
             onshowCity:onshowCity,
-            onHideCity:onHideCity
+            onHideCity:onHideCity,
+            showOrders:showOrders
 
         });
     });
