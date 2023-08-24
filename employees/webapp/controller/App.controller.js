70bc619a0612e1cf4317942de84c8fa77a6d6e4b
Jose Duarte <duartematiasduarte@gmail.com>
2023-08-24 20:10:43 +0000

    15 vo  programa similar al programa con Flexiblecolumlayout y un modelo par escojer el tipo de Flexible

--- employees/webapp/controller/App.controller.js
+++ employees/webapp/controller/App.controller.js
@@ -6,7 +6,39 @@ sap.ui.define(
       "use strict";
   
       return BaseController.extend("logaligroup.employees.controller.App", {
-        onInit() {
+        onAfterRendering: function () {
+          var oView = this.getView();
+    
+          var i18nBundle = oView.getModel("i18n").getResourceBundle();
+        
+ 
+        
+          var oJSONModel = new sap.ui.model.json.JSONModel();
+            oJSONModel.loadData("./localService/mockdata/Employees.json",false);
+            oView.setModel(oJSONModel,"jsonEmployees");
+ 
+ 
+            var oJSONModelCountries = new sap.ui.model.json.JSONModel();
+            oJSONModelCountries.loadData("./localService/mockdata/Countries.json",false);
+            oView.setModel(oJSONModelCountries,"jsonCountries");
+
+            var oJsonModelConfig = new sap.ui.model.json.JSONModel({
+
+              visibleID :true,
+              visibleName :true,
+              visibleCity :false,
+              visibleBtnShowCity :true,
+              visibleBtnHideCity :false
+              
+ 
+            })
+            oView.setModel(oJsonModelConfig,"jsonModelConfig");
+ 
+            var oJsonModelLayout = new sap.ui.model.json.JSONModel();
+            oJsonModelLayout.loadData("./localService/mockdata/Layout.json",false);
+            oView.setModel(oJsonModelLayout,"jsonModelLayout");
+ 
+
         }
       });
     }
--- employees/webapp/controller/MasterEmployee.controller.js
+++ employees/webapp/controller/MasterEmployee.controller.js
@@ -181,41 +181,8 @@ sap.ui.define([
 
     }
 
-        return Controller.extend("logaligroup.employees.controller.View1", {
-            onAfterRendering: function () {
-              var oView = this.getView();
-        
-              var i18nBundle = oView.getModel("i18n").getResourceBundle();
-            
-     
-            
-              var oJSONModel = new sap.ui.model.json.JSONModel();
-                oJSONModel.loadData("./localService/mockdata/Employees.json",false);
-                oView.setModel(oJSONModel,"jsonEmployees");
-     
-     
-                var oJSONModelCountries = new sap.ui.model.json.JSONModel();
-                oJSONModelCountries.loadData("./localService/mockdata/Countries.json",false);
-                oView.setModel(oJSONModelCountries,"jsonCountries");
-
-                var oJsonModelConfig = new sap.ui.model.json.JSONModel({
-
-                  visibleID :true,
-                  visibleName :true,
-                  visibleCity :false,
-                  visibleBtnShowCity :true,
-                  visibleBtnHideCity :false
-                  
-     
-                })
-                oView.setModel(oJsonModelConfig,"jsonModelConfig");
-     
-                var oJsonModelLayout = new sap.ui.model.json.JSONModel();
-                oJsonModelLayout.loadData("./localService/mockdata/Layout.json",false);
-                oView.setModel(oJsonModelLayout,"jsonModelLayout");
-     
-
-            },
+        return Controller.extend("logaligroup.employees.controller.MasterEmployee", {
+          
 
             onValidate:onValidate,
             onFilter:onFilter,
